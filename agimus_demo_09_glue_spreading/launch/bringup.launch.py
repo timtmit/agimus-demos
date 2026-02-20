69fed7d551ef5e29ee397f358f97d1f12a6ce39b
Timothée Carecchio <tcarecchio@gmail.com>
2026-02-20 10:54:36 +0100

    Update bringup.launch.py

--- agimus_demo_09_glue_spreading/launch/bringup.launch.py
+++ agimus_demo_09_glue_spreading/launch/bringup.launch.py
@@ -1,11 +1,10 @@
 from launch import LaunchContext, LaunchDescription
-from launch.actions import OpaqueFunction, RegisterEventHandler, TimerAction
-from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch.actions import OpaqueFunction
 from launch.launch_description_entity import LaunchDescriptionEntity
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.parameter_descriptions import ParameterFile, ParameterValue
+from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration
 
 from agimus_demos_common.launch_utils import (
@@ -32,34 +31,13 @@ def launch_setup(
 
     franka_robot_launch = generate_include_launch(
         "franka_common_lfc.launch.py",
-        extra_launch_arguments={"rviz_config_path": rviz_config_path},
+        extra_launch_arguments={
+            "rviz_config_path": rviz_config_path,
+            "arm_id": arm_id_str,
+            "use_camera": "false",
+            "initial_joint_position": "'-0.013754730661780176 0.08055123973596531 -0.0073070470163592634 -2.1355798783235875 -0.00918790986315326 2.2759845504437037 -2.3657036209762725 0. '",
+        },
     )
-
-    wait_for_non_zero_joints_node = Node(
-        package="agimus_demos_common",
-        executable="wait_for_non_zero_joints_node",
-        parameters=[get_use_sim_time()],
-        output="screen",
-    )
-
-    # agimus_controller_params = PathJoinSubstitution(
-    #     [
-    #         FindPackageShare("agimus_demo_08_collision_avoidance"),
-    #         "config",
-    #         "agimus_controller_params.yaml",
-    #     ]
-    # )
-
-    # agimus_controller_node = Node(
-    #     package="agimus_controller_ros",
-    #     executable="agimus_controller_node",
-    #     parameters=[
-    #         get_use_sim_time(),
-    #         agimus_controller_params,
-    #     ],
-    #     output="screen",
-    #     remappings=[("robot_description", "/robot_description_with_collision")],
-    # )
 
     environment_description = ParameterValue(
         Command(
@@ -112,42 +90,52 @@ def launch_setup(
     )
 
     tf_node = static_transform_publisher_node(
-        frame_id=arm_id_str+"_link0",
+        frame_id=arm_id_str + "_link0",
         child_frame_id="robot_attachment_link",
     )
-    tf_node_plate = static_transform_publisher_node(
-        frame_id="plate_base_link_happypose",
-        child_frame_id="pannel_base_link",
-        xyz=["0.0", "0.0", "0.0"],
-        rot_xyzw=["-0.7071","0.0"," 0.0", "0.7071"],
+
+    # link simulation and real base links
+    tf_world_base = static_transform_publisher_node(
+        frame_id="world",
+        child_frame_id="base",
+    )
+    # tf_node_plate = static_transform_publisher_node(
+    #     frame_id="plate_base_link_happypose",
+    #     child_frame_id="pannel_base_link",
+    #     xyz=["0.0", "0.0", "0.0"],
+    #     rot_xyzw=["-0.7071","0.0"," 0.0", "0.7071"],
+    # )
+
+    tf_node_plate_mpc = static_transform_publisher_node(
+        frame_id=arm_id_str + "_link0",
+        child_frame_id="panel_base_link",
+        xyz=["0.6", "0", "0.0"],
+        rot_xyzw=["0", "0", "0", "1"],
     )
 
+    mpc_params = PathJoinSubstitution(
+        [
+            FindPackageShare("agimus_demo_09_glue_spreading"),
+            "config",
+            "mpc.yaml",
+        ]
+    )
+
+    mpc_node = Node(
+        package="agimus_demo_09_glue_spreading",
+        executable="aligator_MPC",
+        name="aligator_mpc",
+        parameters=[{"config": mpc_params.perform(context)}],
+    )
 
     return [
         franka_robot_launch,
         environment_publisher_node,
         plate_publisher_node,
         tf_node,
-        tf_node_plate,
-        # wait_for_non_zero_joints_node,
-        # RegisterEventHandler(
-        #     event_handler=OnProcessExit(
-        #         target_action=wait_for_non_zero_joints_node,
-        #         on_exit=[
-        #             # agimus_controller_node,
-        #             # obstacle_pose_publisher_node,
-        #         ],
-        #     )
-        # ),
-        # RegisterEventHandler(
-        #     event_handler=OnProcessStart(
-        #         target_action=agimus_controller_node,
-        #         on_start=TimerAction(
-        #             period=5.0,
-        #             actions=[goal_publisher_node],
-        #         ),
-        #     )
-        # ),
+        tf_node_plate_mpc,
+        tf_world_base,
+        mpc_node,
     ]
 
 
